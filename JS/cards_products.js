029db0f24d8b867239020d02ad38f0a4c13cb0ce
Marian Tejeda <marian.tehe9@gmail.com>
2025-08-28 19:21:07 -0600

    Página de experiencias

--- JS/cards_products.js
+++ JS/cards_products.js
@@ -210,3 +210,4 @@ document.addEventListener("DOMContentLoaded", () => {
                 container.innerHTML = '<p class="text-center w-100">Error al cargar los tours. Por favor, inténtalo de nuevo más tarde.</p>';
             }
         });
+    });
